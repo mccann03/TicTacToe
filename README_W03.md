78f2c179566b874f90cafaf3efb102ce71eba44f
Dylan McCann <114199900+mccann03@users.noreply.github.com>
2023-05-04 17:26:16 -0700

    Add files via upload

---
+++ README_W03.md
@@ -0,0 +1,38 @@
+# Overview
+
+{Important! Do not say in this section that this is college assignment. Talk about what you are trying to accomplish as a software engineer to further your learning.}
+
+{Provide a description of the software that you wrote to demonstrate the C++ language.}
+This is a tictactoe program in which two players take turns trying to beat one another.
+
+{Describe your purpose for writing this software.}
+I wrote this program to relax and have fun.
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[● W03 Prove cpp CSE310 Visual Studio Code 2023 05 04 16 13 54](https://www.youtube.com/watch?v=JZ6-Qtdf2tg)
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+For this software I used C++, C++ extension pack, and code run 
+vs code extensions to comile and debug my program.
+
+{Describe the programming language that you used and any libraries.}
+To write my program I used C++, and iostream library for standard 
+to manage standard output and input of it. 
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [Geeks for Geeks](https://www.geeksforgeeks.org/c-plus-plus/)
+- [Microsoft](https://learn.microsoft.com/en-us/cpp/?view=msvc-170)
+
+# Future Work
+
+{Make a list of things that you need to fix, improve, and add in the future.}
+
+- Understanding C++ debugging and code compiling.
+- Better understand standad output for c++
+- Learn about the various types of functions in C++
---
+++ W03_Prove.cpp
@@ -0,0 +1,115 @@
+
+//Setup library for prgram.
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+//Game board array
+char board [3][3] = {1,2,3,4,5,6,7,8,9};
+
+//Variables and declarations
+int PlayerInput;
+int row; 
+int column;
+char PlayerPiece = 'X';
+bool draw = false;
+
+//Layout of gameboard
+void DisplayBoard()
+{
+    cout<<"    |    |    \n";
+    cout<<"  "<<board[0][0]<<" |  "<<board[0][1]<<" |  "<<board[0][2]<<"  \n";
+    cout<<"____|____|____\n";
+    cout<<"    |    |    \n";
+    cout<<"  "<<board[1][0]<<" |  "<<board[1][1]<<" |  "<<board[1][2]<<"  \n";
+    cout<<"____|____|____\n";
+    cout<<"    |    |    \n";
+    cout<<"  "<<board[2][0]<<"  |  "<<board[2][1]<<"   |  "<<board[2][2]<<"  \n";
+    cout<<"    |    |    \n";  
+}
+
+//Function that take player input and places it onto the game board.
+void PlayerTurn(){
+    if (PlayerPiece == 'X'){
+        cout<<"Player 1's turn: ";
+    }
+    else if (PlayerPiece == 'O'){
+        cout<<"Player 2's turn: ";
+    }
+//Code that takes player input.
+    cin>> PlayerInput;
+
+//Code to evaluate a determine which space the player's piece will be place on.   
+    switch(PlayerInput){
+        case 1: row=0; column=0; break;
+        case 2: row=0; column=1; break;
+        case 3: row=0; column=2; break;
+        case 4: row=1; column=0; break;
+        case 5: row=1; column=1; break;
+        case 6: row=1; column=2; break;
+        case 7: row=2; column=0; break;
+        case 8: row=2; column=1; break;
+        case 9: row=2; column=2; break;
+        default:
+            cout<<"Sorry that space isn't on the board."<<endl;
+    }
+
+    //Code to update the locations of the X and O symbols.
+    if (PlayerPiece == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
+        board[row][column] = 'X';
+        PlayerPiece = 'O';
+    }
+    else if (PlayerPiece == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
+        board[row][column] = 'O';
+        PlayerPiece = 'X';
+    }
+    //Code for if the location wants to put a piece has already been filled.
+    else {
+        cout<<"Sorry that place has already been filled. Please choose a different spot."<<endl;
+        PlayerTurn();
+    }
+
+    DisplayBoard();
+}
+
+//Conditions for a game over.
+int GameOver(){
+    for(int i=0;i<3;i++)
+//C`rode that checks for row and column victories.
+    {if (board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
+    return false;
+    }
+//Code checks for diagonal victories.
+    {
+    if (board[0][0] == board[1][1] && board[1][1] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
+    return false;
+    }
+//Determines when to stop the game.
+    for (int i=0; i<3; i++)
+    for(int j=0; j<3; j++)
+    if (board[i][j] != 'X' && board[i][j] != 'O')
+    return true;
+//Check for a draw.
+    {draw = true;
+    return false;}
+}
+
+//The main function where the program begins running.
+int main(){
+    while(GameOver()){
+        DisplayBoard();
+        PlayerTurn();
+        GameOver();
+    }
+//Code that determine what should be printed to the screen 
+//and under what conditions these messages will be shown.
+    if (PlayerPiece == 'O' && draw == false){
+        cout<<"Congratulation! You won Player 1";
+    }
+    else if (PlayerPiece == 'X' && draw == false){
+        cout<<"Congratulation! You won Player 2";
+    }
+    else {
+        cout<<"No winner. Draw";
+    }
+}
